b36212d35d63096f1d9766c4ca568384397fcbaa
tiru-2001 <tirumaleshae709@gmail.com>
2024-06-20 23:43:39 +0530

    added email authentication

--- backend/utils/loademailauth.js
+++ backend/utils/loademailauth.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const loademailauth = () => {
   return nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
+    service: "Outlook365",
+    host: "smtp.office365.com",
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
