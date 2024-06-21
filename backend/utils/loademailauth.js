94c29360591fc0cda80f483f1f0c26261e6b38b2
tiru-2001 <tirumaleshae709@gmail.com>
2024-06-21 12:39:40 +0530

    solved bugs

--- backend/utils/loademailauth.js
+++ backend/utils/loademailauth.js
@@ -5,7 +5,7 @@ const loademailauth = () => {
     service: "Outlook365",
     host: "smtp.office365.com",
     port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_AUTH,
